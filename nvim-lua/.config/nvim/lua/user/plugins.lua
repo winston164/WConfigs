ecc11f1383c20c624e39cecca2a6f422e72852c5
Winston <winstonmartinbendana@gmail.com>
2022-04-06 15:16:50 +0800

    added formatter and diagnostics

--- nvim-lua/.config/nvim/lua/user/plugins.lua
+++ nvim-lua/.config/nvim/lua/user/plugins.lua
@@ -66,6 +66,8 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+
 
   -- Treesitter
   use {
