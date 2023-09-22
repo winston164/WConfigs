5b53751b96c4447a5f3dceae7bdae25f0ebaefa9
Winston Bendana <winstonmartinbendana@gmail.com>
2023-09-22 18:01:43 +0800

    fix: latest nvim-tree config

--- nvim-lua/.config/nvim/lua/user/options.lua
+++ nvim-lua/.config/nvim/lua/user/options.lua
@@ -40,3 +40,7 @@ options.shortmess:append "c"
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+-- Nvim Tree recommendaiton: no netrw (nvim-tree-netrw)
+vim.g.loaded_netrw       = 1
+vim.g.loaded_netrwPlugin = 1
